e15b6f139c781625d9a36a90b24d7576d158cf5e
yeasin911181 <81973822+yeasin911181@users.noreply.github.com>
2021-05-24 02:45:11 +0600

    Newfile.py

--- newfile.py
+++ newfile.py
@@ -1,1 +1,12 @@
-print("you are hacked")
+print("I Fuck You ")
+
+
+print("         -yeasin")
+
+print("     now you can nothin")
+
+print("          you are a fuckboy")
+
+print("               me: yeasin")
+print("                 I am a cracker")
+print("                      ha ha ha")
