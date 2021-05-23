53f45c4d6532e53b2e638f665333d3a1791c2cbb
yeasin911181 <81973822+yeasin911181@users.noreply.github.com>
2021-05-24 03:43:19 +0600

    newfile.py

--- newfile.py
+++ newfile.py
@@ -7,20 +7,39 @@ print("")
 print("")
 print("")
 print("")
+a = input("what is your name")
+
+print("")
+print("")
+
+print("")
+
+print("")
+print("")
+print("")
+print("")
 
 print("I Fuck You ")
 
 
-print("         -yeasin")
+print("        -cracker)
 
 print("     now you can nothing")
 
-print("          you are a fuckboy")
+print(        a," you are a fuckboy")
 
-print("               me: yeasin")
+print("               me: fucker")
 print("                 I am a cracker")
 print("                      ha ha ha")
 
 print("")
 
+print("         unknwon")
+print("")
+print("")
+print("")
+
+print("")
+
+print("")
 print("")
